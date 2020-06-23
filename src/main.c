ad6b59ffdea1885efa42a93232ee0b89544171b4
FireFather <firefather@telenet.be>
2020-06-24 01:10:18 +0200

    src update

---
+++ src/main.c
@@ -0,0 +1,119 @@
+/*
+  nnue-gui is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  nnue-gui is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "common.h"
+
+int WINAPI WinMain(const HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	inst = hInstance;
+
+	if (init_app() == RET_O)
+	{
+		DialogBox(hInstance, "MAIN", NULL, (DLGPROC)dlg_proc);
+	}
+	return RET_O;
+}
+
+short int init_app()
+{
+	h_main = NULL;
+	config = NULL;
+
+	memset(app_path, 0, MAX_FILE_PATH);
+
+	if (GetModuleFileName(NULL, app_path, MAX_FILE_PATH - 384) == 0)
+	{
+		MessageBox(HWND_DESKTOP, "Can't get application path", "Error", MB_OK | MB_ICONERROR);
+		return RET_E;
+	}
+	int i = strlen(app_path) - 1;
+
+	while (i > 0 && app_path[i] != '\\')
+	{
+		i--;
+	}
+
+	if (i == 0)
+	{
+		MessageBox(HWND_DESKTOP, "Can't get application path", "Error", MB_OK | MB_ICONERROR);
+		return RET_E;
+	}
+	app_path[i + 1] = '\0';
+	return RET_O;
+}
+
+BOOL APIENTRY dlg_proc(const HWND h_dlg, const UINT u_msg, const WPARAM w_param, LPARAM l_param)
+{
+	HBITMAP hBMP = 0;
+	HWND hBitmap = 0;
+
+	switch (u_msg)
+	{
+	case WM_INITDIALOG:
+		if (h_main == NULL)
+			h_main = h_dlg;
+
+		hBitmap = GetDlgItem(h_dlg, ID_LOGO);
+		hBMP = LoadBitmap(inst, MAKEINTRESOURCE(ID_BMP));
+		SendMessage(hBitmap, STM_SETIMAGE, (WPARAM)IMAGE_BITMAP, (LPARAM)hBMP);
+		ShowWindow(hBitmap, SW_SHOW);
+
+		load_config();
+		report_engine_config();
+
+		EnableWindow(GetDlgItem(h_main, ID_STOP), FALSE);
+		return TRUE;
+
+	case WM_COMMAND:
+		switch (LOWORD(w_param))
+		{
+		case ID_EXIT:
+			stop_engine_running();
+			EndDialog(h_dlg, 0);
+			break;
+
+		case ID_CONFIG:
+			DialogBox(inst, "CONFIG", NULL, (DLGPROC)dlg_proc_engines);
+			load_config();
+			report_engine_config();
+			break;
+
+		case ID_START:
+			start_thinking();
+			EnableWindow(GetDlgItem(h_main, ID_START), FALSE);
+			EnableWindow(GetDlgItem(h_main, ID_STOP), TRUE);
+			break;
+
+		case ID_STOP:
+			stop_engine();
+			EnableWindow(GetDlgItem(h_main, ID_START), TRUE);
+			EnableWindow(GetDlgItem(h_main, ID_STOP), FALSE);
+			break;
+		default:;
+		}
+		return TRUE;
+
+	case WM_LBUTTONUP:
+		return TRUE;
+
+	case WM_CLOSE:
+		stop_engine_running();
+		EndDialog(h_dlg, 0);
+		return TRUE;
+
+	default:
+		return FALSE;
+	}
+}
---
+++ src/resource.h
@@ -0,0 +1,46 @@
+﻿
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by resources.rc
+//
+
+// main window
+#define ID_UCI_NAME 500
+#define ID_STATUS 501
+#define ID_OUTPUT 502
+#define ID_CONFIG 503
+#define ID_START 504
+#define ID_STOP 505
+#define ID_EXIT 506
+#define ID_BMP 507
+#define ID_LOGO 508
+
+// report config
+#define ID_HASH 600
+#define ID_THREADS 601
+#define ID_SKIP_LOADING_EVAL 602
+#define ID_LOG 603
+
+// config
+#define ID_HASH_CFG 700
+#define ID_THREADS_CFG 701
+#define ID_SKIP_LOADING_EVAL_CFG 702
+#define ID_COMMAND_CFG 703
+#define ID_LOG_CFG 704
+
+#define ID_ENGINE 1000
+#define ID_ENGINE_PATH 1002
+#define ID_ENGINE_SELECT 1003
+#define ID_ENGINE_TB_PATH 1004
+#define ID_ENGINE_TB_SELECT 1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
